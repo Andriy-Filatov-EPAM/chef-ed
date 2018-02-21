b6097a8b8be2eb69dd1d70efecf0430c304f9311
Sergei <ghostshadow@sairanor.ru>
2018-02-21 17:22:34 +0300

    working

---
+++ database/attributes/default.rb
@@ -0,0 +1,5 @@
+default['mysql-multi']['master']="192.168.0.144"
+default['mysql-multi']['slaves']=["192.168.0.145"]
+default['mysql-multi']['server_root_password']="1234567890"
+default['mysql-multi']['server_repl_password']="123456789011"
+default['yum']['mysql56-community']['gpgcheck'] = false
